2df33c5de64209ddb44d648de5f1bb22d207bff4
Danny <mtme13@hotmail.com>
2020-04-28 14:09:46 +0100

    Uncomment types

--- src/app/shared/models/base-playlist.interface.ts
+++ src/app/shared/models/base-playlist.interface.ts
@@ -1,52 +1,52 @@
-// export class BasePlaylistResponseData {
-//   constructor(
-//     public data: BasePlaylist[],
-//     public total: number,
-//     public error?: PlaylistError) { }
+export class BasePlaylistResponseData {
+  constructor(
+    public data: BasePlaylist[],
+    public total: number,
+    public error?: PlaylistError) { }
 
-// }
+}
 
-// // DONE
-// export class BasePlaylist {
-//   constructor(
-//     public id: number,
-//     public title: string,
-//     public is_public: boolean,
-//     public nb_tracks: number,
-//     public link: string,
-//     public picture: string,
-//     public picture_small: string,
-//     public picture_medium: string,
-//     public picture_big: string,
-//     public picture_xl: string,
-//     public checksum: string,
-//     public tracklist: string,
-//     public creation_date: Date,
-//     public type: string
-//   ) { }
+// DONE
+export class BasePlaylist {
+  constructor(
+    public id: number,
+    public title: string,
+    public is_public: boolean,
+    public nb_tracks: number,
+    public link: string,
+    public picture: string,
+    public picture_small: string,
+    public picture_medium: string,
+    public picture_big: string,
+    public picture_xl: string,
+    public checksum: string,
+    public tracklist: string,
+    public creation_date: Date,
+    public type: string
+  ) { }
 
-// }
+}
 
-// export class Creator {
-//   constructor(
-//     public id: number,
-//     public name: string,
-//     public tracklist: string,
-//     public type: string
-//   ) { }
+export class Creator {
+  constructor(
+    public id: number,
+    public name: string,
+    public tracklist: string,
+    public type: string
+  ) { }
 
-// }
+}
 
-// export class PlaylistError {
-//   constructor(public error: ErrorData, ) { }
+export class PlaylistError {
+  constructor(public error: ErrorData, ) { }
 
-// }
+}
 
-// export class ErrorData {
-//   constructor(
-//     public type: string,
-//     public message: string,
-//     public code: number) { }
+export class ErrorData {
+  constructor(
+    public type: string,
+    public message: string,
+    public code: number) { }
 
-// }
+}
 
